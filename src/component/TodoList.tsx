38e64bd5047186d3c45f38e7e372fecf0c6695cf
sugarmse <sarthak.sugar@gmail.com>
2024-05-10 10:32:11 +0545

    UI optimizations

--- src/component/TodoList.tsx
+++ src/component/TodoList.tsx
@@ -3,9 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { addTodo, removeTodo, toggleTodo } from "../state/TodoSlice";
 import { RootState } from "../state/store";
 
-const todolist = (
-
-) => {
+const todolist = () => {
 	const [newTodo, setNewTodo] = useState("");
 	const dispatch = useDispatch();
 	const handleAddTodo = () => {
@@ -18,40 +16,46 @@ const todolist = (
 	const handleRemoveTodo = (todoId: number) => {
 		dispatch(removeTodo(todoId));
 	};
-	  const handleToggleTodo = (todoId: number) => {
-			dispatch(toggleTodo(todoId));
-		};
+	const handleToggleTodo = (todoId: number) => {
+		dispatch(toggleTodo(todoId));
+	};
 	const todos = useSelector((state: RootState) => state.todos);
 
 	return (
 		<>
-			<h1>TO DO LIST</h1>
-
-			<input
-				type="text"
-				value={newTodo}
-				onChange={(e) => setNewTodo(e.target.value)}
-				placeholder="Add new"
-			/>
-			<button className="add" onClick={handleAddTodo}>
-				+
-			</button>
-			<ol>
-				{todos.map((task) => (
-					<div className="todos" key={task.id}>
-						<li
-							style={{
-								textDecoration: task.completed ? "line-through" : "none",
-							}}
-							onClick={() => handleToggleTodo(task.id)}
-						>
-							{task.text}
-						</li>
-						<button onClick={() => handleToggleTodo(task.id)}>✓</button>
-						<button onClick={() => handleRemoveTodo(task.id)}>Remove</button>
+			<div className="dynamic-box">
+				<h1>TO DO LIST</h1>
+				<div className="task-list">
+					<input
+						type="text"
+						value={newTodo}
+						onChange={(e) => setNewTodo(e.target.value)}
+						placeholder="Add new"
+					/>
+				<button className="add" onClick={handleAddTodo}>
+					+
+				</button>
 					</div>
-				))}
-			</ol>
+					
+				<ol className="list">
+					{todos.map((task) => (
+						<div className="todos" key={task.id}>
+							<li
+								style={{
+									textDecoration: task.completed ? "line-through" : "none",
+								}}
+								onClick={() => handleToggleTodo(task.id)}
+								>
+								{task.text}
+							</li>
+								<div className="btns">
+							<button onClick={() => handleToggleTodo(task.id)}>✓</button>
+							<button onClick={() => handleRemoveTodo(task.id)}>X</button>
+						</div>
+					</div>
+					))}
+				</ol>
+			</div>
 		</>
 	);
 };
